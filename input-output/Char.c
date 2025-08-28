8ccc8aa0746687fe727726eaa89d4c4d48aaacea
SHUVO-official <the.shuvo.dev@gmail.com>
2025-08-28 11:44:37 +0600

    Add new file& folder

--- input-output/Char.c
+++ input-output/Char.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
     int main()
         {
-            char ch='A';
-            printf((int)ch);
+            char ch1='A';
+            printf("A=%d\n",(int)ch1);
+
+            char ch2='a';
+            printf("a=%d\n",(int)ch2);
+
+            char ch3='1';
+            printf("1=%d",(int)ch3);
+
+
+
+
         }
