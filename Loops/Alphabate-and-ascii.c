ffb4484d286819994c3739cafe8329df088a9111
SHUVO-official <the.shuvo.dev@gmail.com>
2025-08-28 18:30:28 +0600

    Add New Files

---
+++ Loops/Alphabate-and-ascii.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+    {   
+        printf("Upper case letter: \n");
+        for(int i=65;i<=90;i++) 
+            printf("<--%c-->\n",i);
+            
+        printf("\n\nLower case letter: \n ");
+        for(int i=97;i<=122;i++)
+            printf("<--%c-->\n",i);    
+
+
+    }
