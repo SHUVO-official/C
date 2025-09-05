04b45503798e2697b2d3b60dc97f1060e4b342c3
SHUVO-official <the.shuvo.dev@gmail.com>
2025-09-05 12:29:07 +0600

    Add new Folder

---
+++ Function-&-Pointer/Function/Combination-Permutation.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int fact(int x)
+    {
+        int fact=1;
+        for(int i=2;i<=x;i++)
+            {
+                fact*=i;
+            }
+            return fact;
+    }
+int combination(int n,int r)
+    {
+        int nCr=fact(n)/(fact(r)*fact(n-r));
+        return nCr;
+    }
+int permutation(int n,int r)
+    {
+        int nPr=fact(n)/fact(n-r);
+        return nPr;
+    }        
+int main()
+    {
+        int n,r;
+        scanf("%d%d",&n,&r);
+        int nfact=fact(n);
+        int rfact=fact(r);
+        int nrfact=fact(n-r);
+        int nCr=combination(n,r);
+        printf("Combination: %d\n",nCr);
+        int nPr=permutation(n,r);
+        printf("Permutation: %d",nPr);
+
+        return 0;
+    }
